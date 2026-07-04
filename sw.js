0bc7614823d66058200a2e4f8ebda52a2ae97451
justbispo <38961424+justbispo@users.noreply.github.com>
2026-07-04 21:57:45 +0100

    Bump CACHE_VERSION

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  *     stay fresh.
  * Bump CACHE_VERSION whenever the shell files change to force an update.
  */
-const CACHE_VERSION = "mundial26-v21";
+const CACHE_VERSION = "mundial26-v22";
 const APP_SHELL = [
   "./",
   "./index.html",
